6d3475e033b17b9999f62f245bc37a4b5c0adca9
Jash Gro <91268029+BlackHatDevX@users.noreply.github.com>
2023-08-13 19:31:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = 3000;
 const mongoDB = require("./db");
 app.use(cors());
 // app.use((req, res, next) => {
