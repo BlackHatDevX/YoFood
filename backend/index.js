3fe004e01d4f1a72efbc0977a43d43b167b0b92b
Jash Gro <91268029+BlackHatDevX@users.noreply.github.com>
2023-08-13 19:05:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,6 @@ app.use(cors());
 //   next();
 // });
 mongoDB();
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
